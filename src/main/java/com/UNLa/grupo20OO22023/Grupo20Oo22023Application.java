e5672e36a4c31f0561d3b309c02e73524efc3f4c
PabloDL <pablo.delafore@hotmail.com>
2023-06-01 19:57:13 -0300

    Proyecto vacio creado

---
+++ src/main/java/com/UNLa/grupo20OO22023/Grupo20Oo22023Application.java
@@ -0,0 +1,13 @@
+package com.UNLa.grupo20OO22023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo20Oo22023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo20Oo22023Application.class, args);
+	}
+
+}
